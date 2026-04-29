d9101470222454c67bac1c4ff661006861112af3
Duc-Duy Nguyen <duyndx@gmail.com>
2026-04-29 08:16:20 +0100

    Upload files for DEBS GC 2018 Web app
    Upload files for DEBS GC 2018 Web app: Node JS Leaflet based app

---
+++ script.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+node ./utils/streamer.js --username 000 --interval 2000  --limit 10000 &
+node ./utils/streamer.js --username 012 --interval 2000  --limit 10000 &
+node ./utils/streamer.js --username 016 --interval 2000  --limit 10000 &
+node ./utils/streamer.js --username 017 --interval 2000  --limit 10000 &
+node ./utils/streamer.js --username 020 --interval 2000  --limit 10000 &
+node ./utils/streamer.js --username 025 --interval 2000  --limit 10000 &
+node ./utils/streamer.js --username 030 --interval 2000  --limit 10000 &
+node ./utils/streamer.js --username 032 --interval 2000  --limit 10000 &
+node ./utils/streamer.js --username 037 --interval 2000  --limit 10000 &
+node ./utils/streamer.js --username 042 --interval 2000  --limit 10000 &
+node ./utils/streamer.js --username 062 --interval 2000  --limit 10000 &
+
+
+
+
+
